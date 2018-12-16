75e712607dd57b0d0831299acab6a935ec9f7e79
Ryan Faulhaber <faulhaberryan@gmail.com>
2018-12-16 10:59:26 -0500

    added -r flag

--- fcal.go
+++ fcal.go
@@ -4,29 +4,43 @@ import (
 	"flag"
 	"fmt"
 	"github.com/rfaulhaber/fdate"
+	"log"
+	"os"
 	"strconv"
 	"strings"
 )
 
+var (
+	stderr = log.New(os.Stderr, "fcal: ", 0)
+)
+
 func main() {
 	todayFlag := flag.Bool("t", false, "prints today's date instead of the month")
+	ruralFlag := flag.Bool("r", false, "prints the name of today's day on the Rural Calendar")
 	flag.Parse()
 
-	output := ""
+	if *todayFlag && *ruralFlag {
+		stderr.Fatalln("cannot use -t and -r flags together")
+		os.Exit(1)
+	}
+
+	today := fdate.Today()
+
 
 	if *todayFlag {
-		today := fdate.Today()
-
+		output := ""
 		if today.Month() == 13 {
 			output += today.String() + " - " + fdate.CompDay(today.Day()-1).String() + "\n"
 		} else {
 			output = today.String() + "\n"
 		}
-	} else {
-		output = CreateCalendar(fdate.Today())
-	}
 
-	fmt.Println(output)
+		fmt.Println(output)
+	} else if *ruralFlag {
+		fmt.Println(today.RuralDay())
+	} else {
+		fmt.Println(CreateCalendar(today))
+	}
 }
 
 func CreateCalendar(date fdate.Date) string {
